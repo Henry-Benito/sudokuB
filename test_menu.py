98b68c42f7e73a3987f91529aa20d0c7be4aa49c
rsanchezG <ronald.sanchez@jalasoft.com>
2013-07-05 16:50:10 -0400

    Reviewing folder changes

--- test_menu.py
+++
@@ -1,8 +0,0 @@
-import os
-import unittest
-import settings
-
-class Test_menu(unittest.TestCase):
-    def setUp(self):
-        pass
-    def test_display_mod_settings_should_display_menu_and_get_new_settings_as_a_dict(self):
